3ff655c68b5674de218edaf3c9d76a6fcc34defb
Tahara Mitsuru <almond326@gmail.com>
2021-11-09 11:32:31 +0000

    seventhcommit

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -5,7 +5,7 @@
 @section('title', 'ニュースの新規作成')
 
 {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む　--}}
-@section('title', 'ニュースの新規作成')
+@section('content')
    <div class="container">
        <div class="row">
            <div class="col-md-8 mx-auto">
