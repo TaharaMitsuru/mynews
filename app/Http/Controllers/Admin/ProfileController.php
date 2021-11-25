01c641dc2be6e702a75510ef8282d0d76d44fe78
Tahara Mitsuru <almond326@gmail.com>
2021-11-25 08:01:34 +0000

    課題28

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Profile;
+use App\ProfileHistory;
+use Carbon\Carbon;
 
 class ProfileController extends Controller
 {
@@ -59,6 +61,12 @@ class ProfileController extends Controller
 
       // 該当するデータを上書きして保存する
       $profile->fill($profile_form)->save();
+      
+      //ProfileHistorymodelにCarbonを実装
+      $profile_history = new ProfileHistory();
+      $profile_history->profile_id = $profile->id;
+      $profile_history->edited_at = Carbon::now();
+      $profile_history->save();
 
       return redirect('admin/profile/');
     }
